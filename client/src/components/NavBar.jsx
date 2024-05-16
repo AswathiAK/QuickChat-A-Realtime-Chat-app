c2c1a98efe6011a236596ddd33887cd27d0baf00
Aswathi A K <aswathiak.3@gmail.com>
2024-05-16 19:41:59 +0530

    favicon updated

--- client/src/components/NavBar.jsx
+++ client/src/components/NavBar.jsx
@@ -4,29 +4,28 @@ import { Link } from "react-router-dom";
 import { AuthContext } from "../context/AuthContext";
 import Notification from "./chat/Notification";
 
-
 const NavBar = () => {
   const { user, logoutUser } = useContext(AuthContext);
   return (
-    <Navbar bg="dark" className="mb-4" style={{height:"3.75rem"}}>
+    <Navbar bg="dark" className="mb-4" style={{ height: "3.75rem" }}>
       <Container>
         <h2>
-          <Link to={'/'} className="link-light text-decoration-none">
+          <Link to={"/"} className="link-light text-decoration-none">
             ChatApp
           </Link>
         </h2>
-        {user && 
-          <span className="text-warning">
-            Logged in as {user?.name}
-          </span>
-        }
+        {user && (
+          <span className="text-warning">Logged in as {user?.name}</span>
+        )}
         <Nav>
           <Stack direction="horizontal" gap={3}>
             {user && (
               <>
                 <Notification />
-                <Link onClick={()=>logoutUser()}
-                  to={'/login'} className="link-light text-decoration-none"
+                <Link
+                  onClick={() => logoutUser()}
+                  to={"/login"}
+                  className="link-light text-decoration-none"
                 >
                   Logout
                 </Link>
@@ -34,10 +33,13 @@ const NavBar = () => {
             )}
             {!user && (
               <>
-                <Link to={'/login'} className="link-light text-decoration-none">
+                <Link to={"/login"} className="link-light text-decoration-none">
                   Login
                 </Link>
-                <Link to={'/register'} className="link-light text-decoration-none">
+                <Link
+                  to={"/register"}
+                  className="link-light text-decoration-none"
+                >
                   Register
                 </Link>
               </>
@@ -47,6 +49,6 @@ const NavBar = () => {
       </Container>
     </Navbar>
   );
-}
- 
+};
+
 export default NavBar;
